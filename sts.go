e06c6174a4e55f0261472e8c9b2862d7f94a5292
Yuri Belotserkovsky <ybelotserkovsky@roblox.com>
2023-09-26 19:19:01 -0700

    Add an edgecase where a role name contains an underscore

--- sts.go
+++ sts.go
@@ -16,6 +16,11 @@ func getCallerIdentity(cfg aws.Config) (*sts.GetCallerIdentityOutput, error) {
 }
 
 func AccountAndPermissionSet(cfg aws.Config) (string, string, error) {
+	const (
+		roleSeparator = "_"
+		arnSeparator  = "/"
+	)
+
 	output, err := getCallerIdentity(cfg)
 
 	if err != nil {
@@ -24,18 +29,21 @@ func AccountAndPermissionSet(cfg aws.Config) (string, string, error) {
 
 	account := *output.Account
 	arn := *output.Arn
-	arnParts := strings.Split(arn, "/")
+
+	arnParts := strings.Split(arn, arnSeparator)
 
 	if len(arnParts) != 3 {
 		return "", "", fmt.Errorf("cannot parse arn: %s", arn)
 	}
 
 	role := arnParts[1]
-	roleParts := strings.Split(role, "_")
 
-	if len(roleParts) != 3 {
+	roleParts := strings.Split(role, roleSeparator)
+
+	if len(roleParts) < 3 {
 		return "", "", fmt.Errorf("cannot parse role: %s", role)
 	}
 
-	return account, roleParts[1], nil
+	roleName := strings.Join(roleParts[1:len(roleParts)-1], roleSeparator)
+	return account, roleName, nil
 }
